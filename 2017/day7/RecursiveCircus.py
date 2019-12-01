48be188d44e7e5248f74f41c33336d97c5cb163d
Masz Pecha <eelizaaa@wp.pl>
2019-12-01 20:28:59 +0100

    Synchronization

--- 2017/day7/RecursiveCircus.py
+++ 2017/day7/RecursiveCircus.py
@@ -13,6 +13,17 @@ class RecursiveCircus:
                 leafs.extend(data[1].replace(',', '').split())
         return roots, leafs
 
+    def read2(self):
+        paths = []
+        file = open(self.filename, 'r')
+        for line in file:
+            if "->" not in line:
+                print((line.split()[1]).replace('(,'''))
+                print(line.split()[1].replace('(,''').replace(')', ''))
+                paths.append([line.split()[0], line.split()[1].replace('(,''').replace(')', '')])
+                print([line.split()[0], line.split()[1].replace('(', '').replace(')', '')])
+        return roots, leafs
+
     def findRoot(self, roots, leafs):
         for root in roots:
             if root not in leafs:
@@ -24,3 +35,6 @@ if __name__ == "__main__":
     roots, leafs = rc.read()
     root = rc.findRoot(roots, leafs)
     print(root)
+
+    rc2 = RecursiveCircus("input.txt")
+    rc2.read2()
